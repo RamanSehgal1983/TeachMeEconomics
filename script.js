4f069c1361b825d3e69af665366e5f7a67d108b4
RamanSehgal1983 <raman.sehgal@gmail.com>
2025-08-12 17:54:31 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerBtn = document.getElementById('hamburger-btn');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    hamburgerBtn.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
+});
